19e66aaf20c60c5fcd29e1d6fb1fd18bc2a6462f
dipterix <zw23@rice.edu>
2021-08-06 13:08:59 -0400

    github action auto bot

--- tmp/prepare.R
+++ tmp/prepare.R
@@ -1,5 +1,7 @@
 # Script to add packages
-os <- 'macosx/contrib'
+proj_dir <- normalizePath('.')
+repo_dir <- file.path(proj_dir, get0("repo_dname", ifnotfound = ""))
+
 # https://cran.rstudio.com/src/contrib
 rinfo <- R.Version()
 major <- rinfo$major
@@ -7,35 +9,26 @@ minor <- strsplit(rinfo$minor, '\\.')[[1]][[1]]
 rver <- sprintf('%s.%s', major, minor)
 # install.packages('drat')
 
-proj_dir <- normalizePath('.')
-options(dratRepo = proj_dir)
-drat::addRepo('dipterix', paste0('file:', proj_dir))
+dir.create(repo_dir, showWarnings = FALSE, recursive = TRUE)
+options(dratRepo = repo_dir)
+drat::addRepo('dipterix', paste0('file:', repo_dir))
 
-source_path <- 'tmp/source/'
-github_path <- 'tmp/source/github'
-download_path <- 'tmp/downloads'
-binary_path <- file.path(proj_dir, 'tmp', os)
+if(!file.exists(file.path(repo_dir, 'index.html'))){
+  writeLines("<!doctype html><title>empty</title>", file.path(repo_dir, 'index.html'))
+}
+
+source_path <- './tmp/source/'
+github_path <- './tmp/source/github'
+download_path <- './tmp/downloads'
 unlink(source_path, recursive = TRUE)
-# unlink(binary_path, recursive = TRUE)
-dir.create(binary_path, recursive = TRUE, showWarnings = FALSE)
-dir.create(github_path, recursive = TRUE, showWarnings = FALSE)
 unlink(download_path, recursive = TRUE)
+# dir.create(binary_path, recursive = TRUE, showWarnings = FALSE)
+dir.create(github_path, recursive = TRUE, showWarnings = FALSE)
 dir.create(download_path, recursive = TRUE, showWarnings = FALSE)
 
-# Remove all source packages
-# unlink('src/contrib/', recursive = TRUE)
-dir.create('src/contrib/', recursive = TRUE, showWarnings = FALSE)
-binary_target <- file.path(proj_dir, 'bin', os, 'contrib', rver)
-# unlink(binary_target, recursive = TRUE)
-dir.create(binary_target, recursive = TRUE, showWarnings = FALSE)
 
 
 dependencies <- list(
-  # 'Rcpp' = list(
-  #   url = 'https://github.com/RcppCore/drat/raw/gh-pages/src/contrib/',
-  #   name = 'Rcpp_1.0.4.9.tar.gz',
-  #   type = 'source'
-  # ),
   'dipsaus' = list(
     url = 'https://github.com/dipterix/dipsaus/archive/master.zip',
     type = 'github'
@@ -73,7 +66,6 @@ cache_source <- function(pkg){
   info <- dependencies[[pkg]]
   destfile <- paste0(source_path, info$name)
   download.file(paste0(info$url, info$name), destfile)
-  
 }
 
 cache_github <- function(pkg){
@@ -107,63 +99,38 @@ for(pkg in names(dependencies)){
   
 }
 
+# Build source packages, and prune old packages
 source_packages <- list.files(source_path, pattern = 'gz$', full.names = TRUE)
 for(destfile in source_packages){
-  devtools::build(destfile, path = binary_path, binary = TRUE)
-  drat::insertPackage(destfile)
+  drat::insertPackage(destfile, action = "prune")
 }
 
-binary_packages <- list.files(binary_path, pattern = 'gz$', full.names = TRUE)
-for(destfile in binary_packages){
-  print(destfile)
-  drat::insertPackage(destfile, repodir = '.')
-}
+# Get source path
+source_contrib <- drat:::contrib.url2(repo_dir, 'source')
+source_contrib <- normalizePath(source_contrib, mustWork = TRUE)
+source_packages <- list.files(source_contrib, pattern = "tar\\.gz$", full.names = TRUE)
 
-# # somehow it's installed to mac.binary/contrib, need to copy to el-capitan
-# fdir <- './bin/macosx/mac.binary/contrib/4.0/'
-# fs <- list.files(fdir, full.names = FALSE, recursive = FALSE)
-# for(f in fs){
-#   file.copy(file.path(fdir, f), file.path(binary_target, f), overwrite = TRUE, copy.mode = TRUE, copy.date = TRUE)
-# }
-# 
-# fdir <- './bin/macosx/mac.binary/contrib/4.0/'
-# # dir.create('./bin/macosx/contrib/4.0/', recursive = TRUE)
-# fs <- list.files(fdir, full.names = FALSE, recursive = FALSE)
-# for(f in fs){
-#   file.copy(file.path(fdir, f), file.path('./bin/macosx/contrib/4.0/', f), overwrite = TRUE, copy.mode = TRUE, copy.date = TRUE)
-# }
 
-git2r::add(path = c(list.files('bin', full.names = TRUE), list.files('src', full.names = TRUE)))
-git2r::commit(message = strftime(Sys.time(), 'Rebuild %Y%m%d-%H%M%S'))
-git2r::push()
+
+for(destfile in source_packages){
+  f <- tempfile()
+  if(dir.exists(f)){ unlink(f, recursive = TRUE) }
+  dir.create(f, recursive = TRUE, showWarnings = FALSE)
+  devtools::build(destfile, path = f, binary = TRUE)
+  bin_file <- list.files(f, full.names = TRUE)
   
-# WARNING: Will download all dependent packages
-
-
-# CRAN packages
-cran_packages <- c("abind", "askpass", "assertthat", "backports", "base64enc", "base64url", "BH", "bigmemory.sri", "bit", "bit64", "bitops", "boot", "brew", "callr", "car", "carData", "cellranger", "circular", "class", "cli", "clipr", "clisymbols", "cluster", "codetools", "colorspace", "commonmark", "covr", "crayon", "crosstalk", "curl", "data.table", "desc", "devtools", "digest", "downloader", "DT", "ellipsis", "emmeans", "estimability",  "evaluate", "fansi", "farver", "fastmap", "fftwtools", "filelock", "forcats", "foreign", "freesurferformats", "fs", "future", "future.apply", "future.callr", "ggplot2", "gh", "gifti", "git2r", "globals", "glue", "grid", "gtable", "haven", "hdf5r", "highr", "hms", "htmltools", "htmlwidgets", "httpuv", "httr", "ini", "isoband", "jsonlite", "KernSmooth", "knitr", "labeling", "later", "lattice", "lazyeval", "lifecycle", "listenv", "lme4", "lmerTest", "lsmeans", "magrittr",  "maptools", "markdown", "Matrix", "MatrixModels", "memoise", "mgcv", "microbenchmark", "mime", "minqa", "munsell", "nlme", "nloptr", "nnet", "numDeriv", "openssl", "openxlsx", "oro.nifti", "pbkrtest", "pillar", "pkgbuild", "pkgconfig", "pkgfilecache", "pkgload", "plyr", "praise", "prettyunits", "processx", "progress", "progressr", "promises", "pryr", "ps", "purrr", "qs", "R.matlab", "R.methodsS3", "R.oo", "R.utils", "R6", "RApiSerialize", "rappdirs", "rcmdcheck",  "RColorBrewer", "RcppEigen", "RcppParallel", "RcppRedis", "readr", "readxl", "rematch", "remotes", "reshape2", "reticulate", "rex", "rio", "rlang", "rmarkdown", "RNifti", "roxygen2", "rpart", "rprojroot", "rstudioapi", "rversions", "scales", "servr", "sessioninfo", "shiny", "shinydashboard", "shinyFiles", "shinyjs", "signal", "sourcetools", "sp", "SparseM", "spatial", "startup", "stringi", "stringr", "survival", "synchronicity", "sys", "testthat", "tibble", "tidyselect", "tinytex", "txtq", "usethis", "utf8", "uuid", "vctrs", "viridisLite", "whisker", "withr", "xfun", "xml2", "xopen", "xtable", "yaml", "zip")
-
-
-
-download.packages(cran_packages, repos = "https://cran.rstudio.com/", 
-                  type = 'source', destdir = download_path)
-
-# Add to repo
-cran_extras <- list.files(download_path, pattern = 'gz$', full.names = TRUE)
-for(destfile in cran_extras){
-  drat::insertPackage(destfile)
+  pkginfo <- drat:::getPackageInfo(bin_file)
+  pkgtype <- drat:::identifyPackageType(bin_file, pkginfo)
+  pkgdir <- normalizePath(drat:::contrib.url2(repo_dir, pkgtype, pkginfo["Rmajor"]),
+                          mustWork = FALSE)
+  if(!dir.exists(pkgdir)){
+    dir.create(pkgdir, recursive = TRUE, showWarnings = FALSE)
+  }
+  
+  drat::insertPackage(bin_file, action = 'prune')
 }
 
-# Remove temporary download folder
+# clean
+unlink(source_path, recursive = TRUE)
 unlink(download_path, recursive = TRUE)
-dir.create(download_path, recursive = TRUE, showWarnings = FALSE)
 
-# Download compiled code (work for macosx and windows)
-download.packages(cran_packages, repos = "https://cran.rstudio.com/", 
-                  type = 'binary', destdir = download_path)
-cran_extras <- list.files(download_path, pattern = 'gz$', full.names = TRUE)
-for(destfile in cran_extras){
-  drat::insertPackage(destfile)
-}
-
-unlink(download_path, recursive = TRUE)
